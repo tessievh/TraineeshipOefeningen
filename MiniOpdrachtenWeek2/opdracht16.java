3e83c112e23b5d3acb53117c532d594fc3645175
tessievh <tessievanhintum@gmail.com>
2020-07-29 14:35:22 +0200

    galgje

--- MiniOpdrachtenWeek2/opdracht16.java
+++ MiniOpdrachtenWeek2/opdracht16.java
@@ -1,19 +1,22 @@
 package MiniOpdrachtenWeek2;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class opdracht16 {
 	public static void main(String[] args) {
-		String woord = "hallo";
 		Scanner input = new Scanner(System.in);
-		char w[] = woord.toCharArray();
+		String[] woordString = {"lapjeskat", "donderwolk", "catastrofe", "capitulatie", "pandemonium", "sjacheraar", "murmeren", "scharminkel", "zonnedauw"};
+		Random random = new Random();
+		String woord = "";
+		woord = woordString[random.nextInt(woordString.length)];
+		
 		System.out.println("Welkom bij galgje");
 		System.out.println("Het woord heeft " + woord.length() + " letters");
 		System.out.println("Raad het woord!");
 
-		int counterBeurt = 1;
+		int counterBeurt = 0;
 		int counterGoed = 0;
-
 		String status = "";
 
 		for (int i = 0; i < woord.length(); i++) {
@@ -37,6 +40,10 @@ public class opdracht16 {
 					}
 				} else {
 					System.out.println("Fout");
+						if (counterBeurt == 15) {
+							System.out.println("Helaas, je hebt verloren!");
+							System.out.println("Het woord was: " + woord);
+						}
 
 				}
 				counterBeurt++;
